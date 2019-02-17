d05ecfa7462f1439381b9b46ce2fd8d7a7382bcc
root <root@DESKTOP-D0HP7D2.localdomain>
2019-02-17 13:10:37 +0800

    change common.css

--- button.nim
+++ button.nim
@@ -1,4 +1,4 @@
-import karax / [karax, karaxdsl,vdom, vstyles]
+import karax / [karax, karaxdsl,vdom, vstyles,kbase]
 import dom
 import math
 import jsffi, tables
@@ -81,9 +81,44 @@ proc focusHandler(e:Event, n:VNode) =
 proc login(e:Event, n: VNode) = 
     echo n.dom.nodeName
 
+proc loginButton():VNode = 
+    buildHtml button(class="mdc-button mdc-button--raised", onmousedown = ripple, onblur = blurHandler, onfocus = focusHandler, onclick = login):
+        span(class = "mdc-button__label"):text "LOG IN"
+
+type
+  State = ref object
+    originalTitle: cstring
+    url: Location
+
+proc copyLocation(loc: Location): Location =
+  Location(
+    hash: loc.hash,
+    host: loc.host,
+    hostname: loc.hostname,
+    href: loc.href,
+    pathname: loc.pathname,
+    port: loc.port,
+    protocol: loc.protocol,
+    search: loc.search
+  )
+
+proc newState(): State =
+  State(
+    originalTitle: document.title,
+    url: copyLocation(window.location),
+  )
+
+var state = newState()
+
 proc createDom(): VNode = 
-    buildHtml tdiv(style={width:"100px",height:"100px",margin:"auto",padding:"50px"}):
-            button(class="mdc-button mdc-button--raised", onclick = login, onmousedown = ripple, onblur = blurHandler, onfocus = focusHandler):
-                span(class = "mdc-button__label"):text "LOG IN"
-                
+    buildHtml tdiv(class = "login"):
+        loginButton()
+
+proc onPopState(event: Event) =
+  document.title = state.originalTitle
+  if state.url.href != window.location.href:
+    state = newState() 
+  state.url = copyLocation(window.location)
+
+window.onpopstate = onpopstate              
 setRenderer createDom
